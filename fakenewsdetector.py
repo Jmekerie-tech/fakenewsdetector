4cb245ce1563292cd2db33a2f8498ab1f5fe1dfb
Andrei Cosma <75562343+1cosma1@users.noreply.github.com>
2021-04-18 10:42:13 +0300

    Add files via upload

--- fakenewsdetector.py
+++ fakenewsdetector.py
@@ -11,10 +11,22 @@ def say(audio):
     engine.runAndWait()
 
 def secure_check():
-    link = input("Enter the link:")
+    trust_factor = 0
+    link = input("Enter the news link:")
     if "https://" in link:
-        say("Thw website is secure")
+        trust_factor += 1
     else:
-        say("The website is not secure, there are higher chances for the news to be fakeasdd")
-
+        say("There are higher chances for the news to be fake, becouse of the link...")
+    if "protv" in link:
+        trust_factor += 1
+    if "digi" in link:
+        trust_factor += 1
+    if "observator" in link:
+        trust_factor += 1
+    if "sport.ro" in link:
+        trust_factor += 1
+    if trust_factor == 2:
+        say("The news are over 90 percents true!")
+    else:
+        say("Acording to our algorithm, there are higher chances for the news to be fake.")
 secure_check()
